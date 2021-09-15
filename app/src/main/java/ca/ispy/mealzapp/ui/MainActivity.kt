66e7d2f9b51ca05e4b5a42f07b8c6721833dccac
Brandon <brandonspersonalmailbox@gmail.com>
2021-09-15 17:19:47 -0400

    MealCategory composable completed -> consists of a card, row, column, populated with image (using coil), and text.

--- app/src/main/java/ca/ispy/mealzapp/ui/MainActivity.kt
+++ app/src/main/java/ca/ispy/mealzapp/ui/MainActivity.kt
@@ -1,18 +1,10 @@
-package ca.ispy.mealzapp.ui.meals
+package ca.ispy.mealzapp.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.viewmodel.compose.viewModel
+import ca.ispy.mealzapp.ui.meals.MealsCategoriesScreen
 import ca.ispy.mealzapp.ui.theme.MealzAppTheme
-import ca.ispy.model.response.MealResponse
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/ca/ispy/mealzapp/ui/meals/MealsCategoriesScreen.kt
+++ app/src/main/java/ca/ispy/mealzapp/ui/meals/MealsCategoriesScreen.kt
@@ -1,12 +1,24 @@
 package ca.ispy.mealzapp.ui.meals
 
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import ca.ispy.mealzapp.ui.theme.MealzAppTheme
+import ca.ispy.model.response.MealResponse
+import coil.compose.rememberImagePainter
 
 @Composable
 fun MealsCategoriesScreen() {
@@ -15,10 +27,41 @@ fun MealsCategoriesScreen() {
 
     LazyColumn{
         items(meals) { meal ->
-            Text(text = meal.name)
+            MealCategory(meal)
         }
     }
 
+}
+
+@Composable
+fun MealCategory(meal: MealResponse) {
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        elevation = 2.dp,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 16.dp)
+    ) {
+        Row {
+            Image(
+                painter = rememberImagePainter(meal.imageUrl),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(88.dp)
+                    .padding(4.dp)
+            )
+            Column(
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = meal.name,
+                    style = MaterialTheme.typography.h6
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
